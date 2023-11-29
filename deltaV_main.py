4c3796a9a970b771170029c722a24f6a3b3e28e0
VladDryazgov <vlad1d0@outlook.com>
2023-11-29 23:54:22 +0300

    I started making visuals, but it does not yet work

--- deltaV_main.py
+++ deltaV_main.py
@@ -4,9 +4,10 @@
 import pygame as pg
  
 import deltaV_test
+from deltaV_vis import *
 #settings
 WINDOW_WIDTH = 1000
-WINDOW_HIGHT = 800
+WINDOW_HEIGHT = 800
 
 gameStage = 0
 """Переменная текущего состояния игры. 
@@ -26,8 +27,23 @@ while gameStage==0:
     gameStage = 1
 
 while gameStage==1:
-    deltaV_test.testMain()
+    #deltaV_test.testMain()
+    screen = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    c = Drawable("circle",100,100,1,0xFF0000)
+    cam = Camera()
+    drawer = ScreenDrawer(screen)
+    
+    
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            gameStage="No"
+        else:
+            pass
+        cam.move(event)
+        drawer.draw(cam)
+    pg.display.update()
     clock.tick(30)
+    
 
 
 
--- deltaV_vis.py
+++ deltaV_vis.py
@@ -1,4 +1,45 @@
 """Здесь все что касается нарисовать что-то на экране"""
+import pygame as pg
+import pyautogui
+class Drawable(object):
+    list_of_em=[]
+    """Рисовабельные предметы"""
+    def __init__(self,type,abs_x,abs_y,scale,color):
+        self.type=type
+        self.abs_x=abs_x
+        self.abs_y=abs_y
+        self.scale=scale
+        self.color=color
+        Drawable.list_of_em.append(self)
+class Camera():
+    def __init__(self,x=0,y=0):
+        self.x=x
+        self.y=y
+        self.flag=False
+    def move(self,event):
+        if event:
+            if event.type==pg.MOUSEBUTTONDOWN and self.flag==False:
+                in_x,in_y=self.x,self.y
+                self.flag=True
+                x,y = pyautogui.position()
+            elif event.type==pg.MOUSEBUTTONUP and self.flag==True:
+                self.flag=False
+        x,y=pyautogui.position()
+        in_x,in_y=self.x,self.y
+        if self.flag==True:
+            dx,dy = pyautogui.position()
+            self.x,self.y=in_x+dx-x,in_y+dy-y
 
-class ScreenDrower():
+        
+
+class ScreenDrawer():
     """Класс отвечающий за вывод на экран"""
+    def __init__(self,screen):
+        self.screen=screen
+    def draw(self,cam):
+        for i in Drawable.list_of_em:
+            rel_x,rel_y=i.abs_x+cam.x,i.abs_y+cam.y
+            if i.type=="circle":
+                pg.draw.circle(self.screen, i.color, (rel_x, rel_y), 50*i.scale)
+            
+            
