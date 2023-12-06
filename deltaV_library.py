a481d5138832f2e79f16efb484ade794a978dbe8
VladDryazgov <vlad1d0@outlook.com>
2023-12-06 22:29:13 +0300

    Added something about movement

--- deltaV_library.py
+++ deltaV_library.py
@@ -285,8 +285,10 @@ class Player(GameObject):
             self.angle = self.angle % 360
     
     def move(self, dt):
-        return super().move(dt)
+        super().move(dt)
         #FIXME Изменение скорости из-за работающей тяги
+        self.vx+=self.thrust*self.a_0*math.cos(self.angle*math.pi/180)
+        self.vy+=self.thrust*self.a_0*math.sin(self.angle*math.pi/180)
 
     def dynamic_change(self,event):
         if event.type == pg.KEYDOWN:
